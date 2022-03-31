dd82c066111bd3058b4c46f1138e508fc5016b13
seerat89 <m6375@student.jamk.fi>
2022-03-31 14:19:53 +0300

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import gitHublogo from './assets/GitHub.png';
 
 function App() {
+  const [advice, setAdvice] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => res.json())
+      .then((data) => {
+        setAdvice(data.slip.advice);
+        setLoading(false);
+      });
+  }, []);
+
+  const fetchAdvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => res.json())
+      .then((data) => {
+        setAdvice(data.slip.advice);
+        setLoading(false);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <main className="main">
+      <section className="header">
+        <h1>Random advice generator</h1>
+        <button onClick={() => fetchAdvice()}>New advice</button>
+      </section>
+      <section className="main-section">
+        <div className="corner corner-tl"></div>
+        <div className="corner corner-tr"></div>
+        {loading && (
+          <div className="lds-ripple">
+            <div></div>
+            <div></div>
+          </div>
+        )}
+        {advice && advice}
+        <div className="corner corner-br"></div>
+        <div className="corner corner-bl"></div>
+      </section>
+      <div className="link">
+        <a href="#">
+          <img src={gitHublogo} alt="GitHub Logo" />
         </a>
-      </header>
-    </div>
+      </div>
+      <footer>Taking photos with tablet devices looks weird.</footer>
+    </main>
   );
 }
 
